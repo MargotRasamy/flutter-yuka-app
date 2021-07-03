a321b83ca8b9664696134e2aca5274958edaeff6
margot.rasamy <rasamymargot@gmail.com>
2021-07-03 20:27:50 +0200

    Init project

---
+++ android/app/src/main/kotlin/margot/rasamy/yuka/MainActivity.kt
@@ -0,0 +1,6 @@
+package margot.rasamy.yuka
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+
+@immutable
+class AppColors {
+  static Color black = const Color(0xff000000);
+  static Color white = const Color(0xffFFFFFF);
+  static Color blue = const Color(0xff080040);
+  static Color blueDark = const Color(0xff050027);
+  static Color yellow = const Color(0xffFBAF02);
+  static Color blueLight = const Color(0xff41C7DF);
+  static Color gray1 = const Color(0xffF6F6F8);
+  static Color gray2 = const Color(0xffB8BBC6);
+  static Color gray3 = const Color(0xff6A6A6A);
+  static Color nutrientLevelLow = const Color(0xff63993F);
+  static Color nutrientKevelModerate = const Color(0xff997B3F);
+  static Color nutrientLevelHigh = const Color(0xff993F3F);
+  static Color nutriscoreA = const Color(0xff008b4c);
+  static Color nutriscoreB = const Color(0xff80c142);
+  static Color nutriscoreC = const Color(0xfffeca0b);
+  static Color nutriscoreD = const Color(0xfff58220);
+  static Color nutriscoreE = const Color(0xffef3e22);
+  static Color ecoScoreA = const Color(0xff1E8F4E);
+  static Color ecoScoreB = const Color(0xff2ECC71);
+  static Color ecoScoreC = const Color(0xffFFC900);
+  static Color ecoScoreD = const Color(0xffEF7E1A);
+  static Color ecoScoreE = const Color(0xffE62D19);
+  const AppColors();
+}
